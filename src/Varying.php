8d816865d3a2a3ed778edebadfe070d773fc232e
yunbo <kernelman79@gmail.com>
2019-02-15 13:41:48 +0800

    Add Varying class.

---
+++ src/Varying.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Class Varying
+ *
+ * Author:  Kernel Huang
+ * Mail:    kernelman79@gmail.com
+ * Date:    2/15/19
+ * Time:    1:20 PM
+ */
+
+namespace Common;
+
+
+class Varying
+{
+
+    /**
+     * @param $variable
+     * @return mixed
+     */
+    public static function ifNull($variable) {
+        if (is_array($variable) && empty($variable)) {
+            return null;
+        }
+
+        if (is_string($variable) && $variable == '') {
+            return null;
+        }
+
+        return $variable;
+    }
+}
